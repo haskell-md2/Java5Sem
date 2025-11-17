3a690831da4a8aa768475be7d9c225f95e8c6ff9
Mike <shelliha@s21-edu.ru>
2025-11-17 18:36:14 +0300

    2.6

---
+++ 2/5/A.java
@@ -0,0 +1,9 @@
+public class A {
+    public static int a = 1;
+    public static int b;
+
+    public static void printVars() {
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

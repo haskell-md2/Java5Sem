00d1966fef476608bdc8c56354f85ef05dbbb21a
Mike <shelliha@s21-edu.ru>
2025-11-17 18:08:53 +0300

    2.5

---
+++ 2/5/One.java
@@ -0,0 +1,15 @@
+public class One{ 
+      
+    public static void main (String args[]){
+
+        //1
+        A.printVars();
+
+        //2
+        A obj = new A();
+        obj.printVars();
+
+    }
+}
+
+
